7177b17f22699f99b9f8681525ba5630cdfe7e24
qwerty9927 <tanvo9927@gmail.com>
2022-08-26 12:28:45 +0700

    the first deploy on heroku

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+global.__basedir = __dirname
+const PORT = 5000
+// app.use(cors())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        status: err.status || 500,
+        meg: err.message
+    })
+})
+app.listen(PORT, () => {
+    console.log(`Listen on port ${PORT}`)
+})
