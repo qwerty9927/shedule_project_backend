edde89e17a64f249dae2f03ba2770d33d3fa85a7
qwerty9927 <tanvo9927@gmail.com>
2022-09-18 11:39:10 +0700

    #3 - update

--- src/routes/craw.route.js
+++ src/routes/craw.route.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const Craw = require('../controllers/Craw.controller')
+const verifyTokenAdmin = require('../middlewares/verifyTokenAdmin')
+
+// Middleware
+router.use(verifyTokenAdmin)
 
 // Receive data from crawler
 router.post('/', Craw.receiveData)
